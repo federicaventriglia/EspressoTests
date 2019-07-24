5dfb8e8c175290707d5ab2d1e4135a313fa9d01c
Federica Ventriglia <federica.ventriglia@yahoo.it>
2019-07-24 14:11:48 +0200

    Analysis
    JMP Analysis of Standard Test Executions

---
+++ Results/standard_tests/filter.py
@@ -0,0 +1,6 @@
+import codecs
+path='/Users/federicaventriglia/Documents/Universita/TESI/EspressoTests/EspressoTests/Results/standard_tests/simply-do-app/kdk.android.simplydo.C2.SortItemsC2.html'
+file=codecs.open(path,"rb")
+file1=file.read()
+file1=str(file1)
+print(file1)
